cfccde1a995b29a09bf5e3ed3dc04fd55d21bae5
Meghamitra-22 <chowdhurymeghamitra@gmail.com>
2026-01-13 23:42:49 +0530

    Delete pid_ziegler.m

--- pid_ziegler.m
+++
@@ -1,7 +0,0 @@
-s = tf('s');                       % Define 's' as Laplace variable
-W = 1 / (s * (s + 2) * (s + 3));   % Define the transfer function W(s)
-
-numW = W.Numerator{1};             % Extract numerator coefficients of W(s)
-denW = W.Denominator{1};           % Extract denominator coefficients of W(s)
-
-Kc = 10;                           % Controller gain set to 10
